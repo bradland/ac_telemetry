e816be310d9ad8d6f90b7515067db063460b2b51
Brad Landers <brad@bradlanders.com>
2017-12-16 19:42:33 -0500

    Output sensible lap data.

--- lib/ac_telemetry/record_formatters/rt_lap.rb
+++ lib/ac_telemetry/record_formatters/rt_lap.rb
@@ -1,10 +1,11 @@
 require 'ac_telemetry/record_formatters/base'
+require 'ap'
 
 module ACTelemetry
   module RecordFormatters
     class RTLap < Base
       def format(record)
-        record.to_binary_s
+        "car id: %s, lap: %d driver: %s, car: %s, time: %0.3f" % clean_array([record.car_identifier_number, record.lap, record.driver_name, record.car_name, record.time.to_f/1000])
       end
     end
   end
